3880a7e1f6dec2dd6f36f88821a7775369ea4d19
michaelmjh <74016689+michaelmjh@users.noreply.github.com>
2022-05-09 14:09:15 +0100

    final code

---
+++ team_class/src/team.py
@@ -0,0 +1,21 @@
+class Team:
+    def __init__(self, name, players, coach):
+        self.name = name
+        self.players = players
+        self.coach = coach
+        self.points = 0
+
+    def add_player(self, player_name):
+        self.players.append(player_name)
+
+    def has_player(self, player_name):
+        for player in self.players:
+            if player == player_name:
+                return True
+                
+        return False
+
+    def play_game(self, win_state):
+        if win_state:
+            self.points += 3
+
